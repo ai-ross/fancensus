395929a71e0911ae7de2168c9536ae4fbaeb52c7
Ross Feeley <infiniterossi@gmail.com>
2025-02-15 18:33:40 +0000

    Initialise project & dependencies

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100 py-8">
+    <div class="container mx-auto">
+      <h1 class="text-3xl font-bold text-center mb-8">Dashboard</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
